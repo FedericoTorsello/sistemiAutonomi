cf6bf6bf86fc0e7573ccff2c3d393c457544a3a3
Federico Torsello <federico.torsello@studio.unibo.it>
2014-12-11 12:14:05 +0100

    main

--- it.autonomi.scopone/src/it/autonomi/scopone/main/Main.java
+++ it.autonomi.scopone/src/it/autonomi/scopone/main/Main.java
@@ -15,13 +15,46 @@ public class Main {
 	static ArrayList<Card> finalDeck;
 
 	public static void main(String[] args) {
-		initialDeck = new ArrayList<Card>();
-		finalDeck = new ArrayList<Card>();
-		generationDeck(Seed.BASTONI);
-		generationDeck(Seed.COPPE);
-		generationDeck(Seed.DENARI);
-		generationDeck(Seed.SPADE);
-		randomCard();
+//		int bast = 0;
+//		int coppe = 0;
+//		int den = 0;
+//		int spad = 0;
+//		int var = 2;
+//		int basto = 0;
+//		int number = 1000000;
+//		for (int i = 0; i <= number; i++) {
+////			if (i % (number/100) == 0) {
+//			if (i % (number/10) == 0) {
+//				System.out.println(i / (number/100) + "%");
+//			}
+			initialDeck = new ArrayList<Card>();
+			finalDeck = new ArrayList<Card>();
+			generationDeck(Seed.BASTONI);
+			generationDeck(Seed.COPPE);
+			generationDeck(Seed.DENARI);
+			generationDeck(Seed.SPADE);
+			randomCard();
+//			if (finalDeck.get(var).getSeed() == Seed.BASTONI) {
+//				bast++;
+////				if (finalDeck.get(var).getNumber() == 1) {
+////					basto++;
+////				}
+//			}
+//			if (finalDeck.get(var).getSeed() == Seed.COPPE) {
+//				coppe++;
+//			}
+//			if (finalDeck.get(var).getSeed() == Seed.DENARI) {
+//				den++;
+//			}
+//			if (finalDeck.get(var).getSeed() == Seed.SPADE) {
+//				spad++;
+//			}
+//		}
+//		System.out.println(bast);
+//		System.out.println(coppe);
+//		System.out.println(den);
+//		System.out.println(spad);
+//		System.out.println((bast + coppe + den + spad));
 	}
 
 	public static void generationDeck(Seed inputSeed) {
@@ -33,13 +66,13 @@ public class Main {
 
 	// serve a mischiare le carte
 	public static void randomCard() {
+		Random rnd = new Random();
 		for (int i = 0; i < NUMCARDMAX; i++) {
-			int r = (int) (Math.random() * i);
+			int r = (int) (rnd.nextInt(i + 1));
 			finalDeck.add(r, initialDeck.get(i));
 		}
-		for (int i = 0; i < NUMCARDMAX; i++) {
-			System.out.println(finalDeck.get(i).getCardStr());
-		}
+		 for (int i = 0; i < NUMCARDMAX; i++) {
+		 System.out.println(finalDeck.get(i).getCardStr());
+		 }
 	}
-
 }
